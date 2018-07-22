23126e07ddd184e5d00cd6e9fd423191586ea660
dannyhuang <kuanhsuh@gmail.com>
2018-07-22 23:18:14 +0800

    Finish Global Footer

--- footer.php
+++ footer.php
@@ -14,19 +14,60 @@
 
 <!-- FOOTER
 ==================================================== -->
-<footer class="mt-5">
+<!-- <footer class="mt-5">
     <div class="container">
       <div class="row justify-content-center">
         <p>
-				Copyright &copy; <?php bloginfo('name'); ?> - <?php echo date('Y')?>
+				Copyright &copy; <?php //bloginfo('name'); ?> - <?php //echo date('Y')?>
         </p>
       </div>
-    </div> <!-- container -->
+    </div>
+</footer> -->
+<footer class="p-5">
+  <div class="container">
+    <div class="row">
+      <div class="col-6">
+        <h3 class="mb-4">ShowRoom</h3>
+        <ul>
+          <li>高雄市三民區義華路777號</li>
+          <li>Mon-Fri/AM10:00-PM7:00</li>
+          <li class="mb-3">Sun 預約制，請來電預</li>
+          <li>T 07 777-333-8888</li>
+          <li>F 07 777-333-8888</li>
+          <li class="mb-3">E info@pembridge.com</li>
+          <li>
+            <a href="#">
+            MAP
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div class="col-6">
+        <h3 class="mb-4">Follow Us</h3>
+        <p>
+          <a href="https://www.facebook.com/">
+            <ion-icon name="logo-facebook" class="icon"></ion-icon>
+          </a>
+          <a href="https://www.facebook.com/">
+            <ion-icon name="logo-instagram" class="icon"></ion-icon>
+          </a>
+        </p>
+      </div>
+    </div>
+    <div class="row justify-content-center">
+        <p>
+				Copyright &copy; <?php bloginfo('name');?> - <?php echo date('Y') ?>
+        </p>
+      </div>
+  </div>
 </footer>
-</div> <!-- content -->
+</div>
 
-<?php wp_footer(); ?>
-<script src="<?php bloginfo('template_directory'); ?>/assets/js/bootstrap.min.js"></script>
-<script src="<?php bloginfo('template_directory'); ?>/assets/js/app.js"></script>
+<!-- content -->
+
+<?php wp_footer();?>
+<script src="<?php bloginfo('template_directory');?>/assets/js/bootstrap.min.js"></script>
+<script src="https://unpkg.com/ionicons@4.2.4/dist/ionicons.js"></script>
+<script src="<?php bloginfo('template_directory');?>/assets/js/app.js"></script>
 </body>
 </html>
